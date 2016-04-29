7bd1eabbf96b75cca9917cf4f3b82fe62d22dd26
sophiakc <sophiakc@gmail.com>
2016-04-29 16:24:42 -0700

    failed in doing rake db:migrate

--- app/models/companies.rb
+++ app/models/companies.rb
@@ -1,4 +1,4 @@
-class Companies < ActiveRecord::Base
+class Company < ActiveRecord::Base
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
--- db/migrate/20160429230725_devise_create_companies.rb
+++ db/migrate/20160429230725_devise_create_companies.rb
@@ -1,4 +1,4 @@
-class DeviseCreateCompanies < ActiveRecord::Migration
+class CreateCompanies < ActiveRecord::Migration
   def change
     create_table(:companies) do |t|
       ## Database authenticatable
@@ -19,24 +19,8 @@ class DeviseCreateCompanies < ActiveRecord::Migration
       t.string   :current_sign_in_ip
       t.string   :last_sign_in_ip
 
-      ## Confirmable
-      # t.string   :confirmation_token
-      # t.datetime :confirmed_at
-      # t.datetime :confirmation_sent_at
-      # t.string   :unconfirmed_email # Only if using reconfirmable
-
-      ## Lockable
-      # t.integer  :failed_attempts, default: 0, null: false # Only if lock strategy is :failed_attempts
-      # t.string   :unlock_token # Only if unlock strategy is :email or :both
-      # t.datetime :locked_at
-
-
       t.timestamps null: false
 
-      ## Profile information
-      t.string  :name
-      t.integer :size
-      t.text    :activity
     end
 
     add_index :companies, :email,                unique: true
