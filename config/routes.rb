baa53906f1e7e8bb57a4d1d580d62b95f0bfb2c6
sophiakc <sophiakc@gmail.com>
2016-04-29 18:13:56 -0700

    config scoped views for companies to true since we need different views per models in the app

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,6 @@
 Rails.application.routes.draw do
 
-  # root to: "home#index"
-  devise_for :companies
+  devise_for :companies, controllers: { sessions: "companies/sessions" }
   resources :jobs
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
